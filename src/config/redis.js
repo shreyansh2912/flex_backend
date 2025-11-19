77e4683fd8386504b80be4397f490f566bae6fb2
Shreyansh Shah <shreyanshshah2912@gmail.com>
2025-11-19 15:36:45 +0530

    redis token fix

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,25 +2,22 @@ const Redis = require('ioredis');
 
 let redis;
 
-if (process.env.REDIS_URL) {
-  // For production (Render, Railway, etc.)
-  redis = new Redis(process.env.REDIS_URL);
-} else {
-  // Local development
+if (process.env.REDIS_URL && process.env.REDIS_TOKEN) {
+  
   redis = new Redis({
-    host: '127.0.0.1',
-    port: 6379,
-    // password: 'yourpassword', // uncomment if needed
-  });
+    url: process.env.REDIS_URL,
+    token: process.env.REDIS_TOKEN,
+  })
+}else{
+  redis = new Redis(process.env.REDIS_URL);
 }
+  // Test connection
+  redis.on('connect', () => {
+    console.log('Redis connected successfully');
+  });
 
-// Test connection
-redis.on('connect', () => {
-  console.log('Redis connected successfully');
-});
+  redis.on('error', (err) => {
+    console.error('Redis connection error:', err.message);
+  });
 
-redis.on('error', (err) => {
-  console.error('Redis connection error:', err.message);
-});
-
-module.exports = redis;
+  module.exports = redis;
